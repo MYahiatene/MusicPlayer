3fac1e247dc9f621501e794aa99388bdf766bcb1
ymokrane <ymokrane@techfak.de>
2019-11-04 12:49:43 +0100

    Task 1929
    -Abspielen der Playlist implementiert, mp3s hören nie auf zu spielen
    
    - closes #1929 @1.0h

--- src/main/java/de/techfak/gse/ymokrane/GSERadio.java
+++ src/main/java/de/techfak/gse/ymokrane/GSERadio.java
@@ -13,15 +13,16 @@ public final class GSERadio {
     public static void main(final String... args) {
         //Erzeugen meines parsers //
         final PathParser parser = new PathParser(args);
-        final String pfad = parser.getPfad();
 
         //Einlesen der mp3s aus dem Ornder und speichern in einer Playlist(geshuffled) //
 
         final List<File> playlist = parser.getPlaylist();
 
         //erstellt Music Player //
-        final MusicPlayer player = new MusicPlayer();
-        //player.play();
+        final MusicPlayer player = new MusicPlayer(playlist);
+
+        //Abspielen der mp3s //
+        player.playSongs();
 
 
         System.out.println("Hello ymokrane!");
--- src/main/java/de/techfak/gse/ymokrane/MusicPlayer.java
+++ src/main/java/de/techfak/gse/ymokrane/MusicPlayer.java
@@ -2,31 +2,58 @@ package de.techfak.gse.ymokrane;
 
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+
+import java.io.File;
+import java.util.List;
 
 public class MusicPlayer {
 
-   // private String pfad;
+    private int playlistIndex = 0;
+    private List<File> playlist;
+
 
     private MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
 
     private MediaPlayer mediaPlayer = mediaPlayerFactory.mediaPlayers().newMediaPlayer();
 
-    /*public MusicPlayer() {
 
-        //this.pfad = pfad;
+    public MusicPlayer(final List<File> playlist) {
 
-    }*/
+        this.playlist = playlist;
 
-    public void playSong(final String mp3) {
+    }
+
+
+    public void playSongs() {
+
 
         mediaPlayer.submit(new Runnable() {
             @Override
             public void run() {
-                //mediaPlayer.media().play();
+                mediaPlayer.media().play(playlist.get(playlistIndex).toString());
             }
+        });
+
+        mediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+
+            @Override
+            public void finished(final MediaPlayer mediaPlayer) {
+                if (playlistIndex < playlist.size() - 1) {
+                    playlistIndex += 1;
+                } else {
+                    playlistIndex = 0;
+                }
+                mediaPlayer.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        mediaPlayer.media().play(playlist.get(playlistIndex).toString());
+                    }
+                });
+            }
+
         });
 
 
     }
-
 }
