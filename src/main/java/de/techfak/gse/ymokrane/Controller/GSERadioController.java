b43e7d497148f6e1bd11c85ee15b0e92b14bcbf1
Mokrane Yahiatene <ymokrane@techfak.uni-bielefeld.de>
2019-11-26 18:17:04 +0100

    Implementierung des Fxmloaders

--- src/main/java/de/techfak/gse/ymokrane/Controller/GSERadioController.java
+++ src/main/java/de/techfak/gse/ymokrane/Controller/GSERadioController.java
@@ -1,4 +1,27 @@
 package de.techfak.gse.ymokrane.Controller;
 
-public class GSERadioController {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class GSERadioController extends Application {
+
+    public static void main(String... args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/GSERadioView.fxml"));
+        stage.setTitle("GUI");
+        stage.setScene(new Scene(root, 800, 500));
+        stage.show();
+    }
 }
+
