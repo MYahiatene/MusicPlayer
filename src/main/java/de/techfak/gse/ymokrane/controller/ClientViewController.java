3c801c8270b62324d3ea7ef3314151512ffd3e48
ymokrane <ymokrane@techfak.uni-bielefeld.de>
2020-01-10 01:35:40 +0100

    Task 10786:
    -Bei erfolgereicher Verbindung wird entsprechende Meldung in Gui angezeigt.
    Bei misslungener Anmeldung wird Fehler ausgespuckt
    
    closes #10786 @1.5h

--- src/main/java/de/techfak/gse/ymokrane/controller/ClientViewController.java
+++ src/main/java/de/techfak/gse/ymokrane/controller/ClientViewController.java
@@ -3,12 +3,15 @@ package de.techfak.gse.ymokrane.controller;
 import de.techfak.gse.ymokrane.model.WebClient;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 
 
-public class ClientViewController {
+public class ClientViewController implements PropertyChangeListener {
     @FXML
     Button button1;
 
@@ -18,15 +21,30 @@ public class ClientViewController {
     @FXML
     TextField port;
 
+    @FXML
+    Label connectionStatus;
+
     public void connectServer() throws IOException {
+
         if (ip.getCharacters().length() != 0 && ip.getCharacters().length() != 0) {
+            WebClient client = new WebClient();
+            client.addPropertyChangeListener(this);
             String[] args = new String[3];
             args[0] = "http://";
             args[1] = ip.getCharacters().toString();
             args[2] = port.getCharacters().toString();
-            WebClient.main(args);
+            client.client(args);
+
         } else {
             throw new IOException("Eingabe falsch");
         }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("connected")) {
+            connectionStatus.setText("connection established!");
+        }
+
     }
 }
--- src/main/java/de/techfak/gse/ymokrane/model/WebClient.java
+++ src/main/java/de/techfak/gse/ymokrane/model/WebClient.java
@@ -1,5 +1,7 @@
 package de.techfak.gse.ymokrane.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,22 +11,38 @@ import java.net.http.HttpResponse;
 
 public final class WebClient {
 
+    private PropertyChangeSupport support;
 
     public WebClient() {
+
+        this.support = new PropertyChangeSupport(this);
+
     }
+
+    public void addPropertyChangeListener(final PropertyChangeListener observer) {
+        support.addPropertyChangeListener(observer);
+    }
+
 
     /**
      * Queries and prints christmas markets in Berlin.
      *
      * @param args commandline arguments given on execution
      */
-    public static void main(final String... args) {
+    public void client(final String... args) {
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(args[0] + args[1] + ":" + args[2]))
             .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                support.firePropertyChange("not connected", true, false);
+            }
+            if (response.statusCode() == 200) {
+                support.firePropertyChange("connected", true, false);
+            }
             System.out.println("HTTP-Status: " + response.statusCode());
             System.out.println("Response Body: " + response.body());
         } catch (IOException | InterruptedException e) {
